f4a21d9d8c1350dccf5f6fe7017dc355947c9412
Dsgnr <opensource@realtypremium.com>
2014-10-06 20:15:54 +0530

    Experimenting with vendor prefixes, though temporarily disabled

--- tasks/converter/less_conversion.rb
+++ tasks/converter/less_conversion.rb
@@ -177,15 +177,16 @@ class Converter
       file   = deinterpolate_vararg_mixins(file)
       file   = replace_calculation_semantics(file)
       file   = replace_file_imports(file)
-      file   = replace_vendor_prefixes(file)
+      #file   = replace_vendor_prefixes(file)
       file
     end
 
     def replace_vendor_prefixes file
-      match1 =  file.match /(-webkit-.*?;)|(-khtml-.*?;)|(-ms-.*?;)|(-moz-.*?;)|(-o-.*?;)/
-      #match2 = match1.sub! /(-webkit-.*?)|(-khtml-.*?)|(-ms-.*?)|(-moz-.*?)|(-o-.*?)/, "@include " unless match1.nil?
-      puts match1
-
+      match1 =  file.match(/(-webkit-.*?;)|(-khtml-.*?;)|(-ms-.*?;)|(-moz-.*?;)|(-o-.*?;)/).to_s
+      match2 = match1.sub!(/(-webkit-.*?)|(-khtml-.*?)|(-ms-.*?)|(-moz-.*?)|(-o-.*?)/, "@include ").to_s unless match1.nil?
+      property_val = match2.match(/(?<=\:)(.*?)(?=\;)/) unless match2.nil?
+      compassd = match2.sub! /(:.*?;)/, "(#{property_val});" unless match2.nil?
+      compassd
     end
 
     def sass_fn_exists(fn)
